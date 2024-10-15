ce5d22d7ce448422cf27ec2a8b32e165b2602bb7
Quinten <qdamsma@gmail.com>
2024-10-15 18:40:12 +0200

    Opschonen reset teamswapper

--- lib/pages/teamswapper.dart
+++ lib/pages/teamswapper.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter_f1_app/pages/teamswapper_functies.dart';
 import 'package:flutter_f1_app/services/teamswapper_data.dart';
 
 
@@ -9,39 +10,19 @@ class Teamswapper extends StatefulWidget {
 }
 
 class _TeamswapperState extends State<Teamswapper> {
-  bool coureursZichtbaarheid = true;
-  bool overzicht = false;
-  int selectedIndex = 1;
   List<Image?> droppedImage = List.generate(20, (index) => null);
 
   // Functie om alle coureurs te resetten naar de grid
   void resetKnop(){
     setState(() {
       droppedImage = List.generate(20, (index) => null);
-      drivers = [
-    {'name': 'Max Verstappen', 'image': Image.asset('lib/assets/images/drivers/Max.png'), 'isVisible': true, 'ELO' : 32.98},
-    {'name': 'Sergio Perez','image': Image.asset('lib/assets/images/drivers/Sergio.png'), 'isVisible': true, 'ELO' : 29.97},
-    {'name': 'Charles Leclerc','image': Image.asset('lib/assets/images/drivers/Charles.png'), 'isVisible': true, 'ELO' : 30.54},
-    {'name': 'Carlos Sainz','image': Image.asset('lib/assets/images/drivers/Carlos.png'), 'isVisible': true, 'ELO' : 29.16},
-    {'name': 'Lewis Hamilton','image': Image.asset('lib/assets/images/drivers/Lewis.png'), 'isVisible': true, 'ELO' : 32.98},
-    {'name': 'George Russel','image': Image.asset('lib/assets/images/drivers/George.png'), 'isVisible': true, 'ELO' : 26.53},
-    {'name': 'Oscar Piastri','image': Image.asset('lib/assets/images/drivers/Oscar.png'), 'isVisible': true, 'ELO' : 27.00},
-    {'name': 'Lando Norris','image': Image.asset('lib/assets/images/drivers/Lando.png'), 'isVisible': true, 'ELO' : 28.16},
-    {'name': 'Fernando Alonso','image': Image.asset('lib/assets/images/drivers/Fernando.png'), 'isVisible': true, 'ELO' : 28.90},
-    {'name': 'Lance Stroll','image': Image.asset('lib/assets/images/drivers/Lance.png'), 'isVisible': true, 'ELO' : 27.59},
-    {'name': 'Daniel Ricciardo','image': Image.asset('lib/assets/images/drivers/Daniel.png'), 'isVisible': true, 'ELO' : 30.50},
-    {'name': 'Yuki Tsunoda','image': Image.asset('lib/assets/images/drivers/Yuki.png'), 'isVisible': true, 'ELO' : 26.75},
-    {'name': 'Nico Hülkenberg','image': Image.asset('lib/assets/images/drivers/Nico.png'), 'isVisible': true, 'ELO' : 28.65},
-    {'name': 'Kevin Magnussen','image': Image.asset('lib/assets/images/drivers/Kevin.png'), 'isVisible': true, 'ELO' : 27.90},
-    {'name': 'Esteban Ocon','image': Image.asset('lib/assets/images/drivers/Esteban.png'), 'isVisible': true, 'ELO' : 28.61},
-    {'name': 'Pierre Gasly','image': Image.asset('lib/assets/images/drivers/Pierre.png'), 'isVisible': true, 'ELO' : 28.80},
-    {'name': 'Alexander Albon','image': Image.asset('lib/assets/images/drivers/Alex.png'), 'isVisible': true, 'ELO' : 29.21},
-    {'name': 'Logan Sargeant','image': Image.asset('lib/assets/images/drivers/Logan.png'), 'isVisible': true, 'ELO' : 26.25},
-    {'name': 'Valterri Bottas','image': Image.asset('lib/assets/images/drivers/Valterri.png'), 'isVisible': true, 'ELO' : 31.90},
-    {'name': 'Guanyu Zhou','image': Image.asset('lib/assets/images/drivers/Guanyu.png'), 'isVisible': true, 'ELO' : 26.50},
-  ];
+      drivers = TeamswapperFuncties.resetDrivers();
     });
   }
+
+  bool coureursZichtbaarheid = true;
+  bool overzicht = false;
+  int selectedIndex = 1;
 
   // Functie die ervoor zorgt dat er willekeurige coureurs bij een team worden gezet als je op de 'willekeurig toewijzen' knop klik
   void randomCoureurs() {
---
+++ lib/pages/teamswapper_functies.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+class TeamswapperFuncties {
+  static List<Map<String, dynamic>> resetDrivers() {
+    return [
+      {
+        'name': 'Max Verstappen',
+        'image': Image.asset('lib/assets/images/drivers/Max.png'),
+        'isVisible': true,
+        'ELO': 32.98
+      },
+      {
+        'name': 'Sergio Perez',
+        'image': Image.asset('lib/assets/images/drivers/Sergio.png'),
+        'isVisible': true,
+        'ELO': 29.97
+      },
+      {
+        'name': 'Charles Leclerc',
+        'image': Image.asset('lib/assets/images/drivers/Charles.png'),
+        'isVisible': true,
+        'ELO': 30.54
+      },
+      {
+        'name': 'Carlos Sainz',
+        'image': Image.asset('lib/assets/images/drivers/Carlos.png'),
+        'isVisible': true,
+        'ELO': 29.16
+      },
+      {
+        'name': 'Lewis Hamilton',
+        'image': Image.asset('lib/assets/images/drivers/Lewis.png'),
+        'isVisible': true,
+        'ELO': 32.98
+      },
+      {
+        'name': 'George Russel',
+        'image': Image.asset('lib/assets/images/drivers/George.png'),
+        'isVisible': true,
+        'ELO': 26.53
+      },
+      {
+        'name': 'Oscar Piastri',
+        'image': Image.asset('lib/assets/images/drivers/Oscar.png'),
+        'isVisible': true,
+        'ELO': 27.00
+      },
+      {
+        'name': 'Lando Norris',
+        'image': Image.asset('lib/assets/images/drivers/Lando.png'),
+        'isVisible': true,
+        'ELO': 28.16
+      },
+      {
+        'name': 'Fernando Alonso',
+        'image': Image.asset('lib/assets/images/drivers/Fernando.png'),
+        'isVisible': true,
+        'ELO': 28.90
+      },
+      {
+        'name': 'Lance Stroll',
+        'image': Image.asset('lib/assets/images/drivers/Lance.png'),
+        'isVisible': true,
+        'ELO': 27.59
+      },
+      {
+        'name': 'Daniel Ricciardo',
+        'image': Image.asset('lib/assets/images/drivers/Daniel.png'),
+        'isVisible': true,
+        'ELO': 30.50
+      },
+      {
+        'name': 'Yuki Tsunoda',
+        'image': Image.asset('lib/assets/images/drivers/Yuki.png'),
+        'isVisible': true,
+        'ELO': 26.75
+      },
+      {
+        'name': 'Nico Hülkenberg',
+        'image': Image.asset('lib/assets/images/drivers/Nico.png'),
+        'isVisible': true,
+        'ELO': 28.65
+      },
+      {
+        'name': 'Kevin Magnussen',
+        'image': Image.asset('lib/assets/images/drivers/Kevin.png'),
+        'isVisible': true,
+        'ELO': 27.90
+      },
+      {
+        'name': 'Esteban Ocon',
+        'image': Image.asset('lib/assets/images/drivers/Esteban.png'),
+        'isVisible': true,
+        'ELO': 28.61
+      },
+      {
+        'name': 'Pierre Gasly',
+        'image': Image.asset('lib/assets/images/drivers/Pierre.png'),
+        'isVisible': true,
+        'ELO': 28.80
+      },
+      {
+        'name': 'Alexander Albon',
+        'image': Image.asset('lib/assets/images/drivers/Alex.png'),
+        'isVisible': true,
+        'ELO': 29.21
+      },
+      {
+        'name': 'Logan Sargeant',
+        'image': Image.asset('lib/assets/images/drivers/Logan.png'),
+        'isVisible': true,
+        'ELO': 26.25
+      },
+      {
+        'name': 'Valterri Bottas',
+        'image': Image.asset('lib/assets/images/drivers/Valterri.png'),
+        'isVisible': true,
+        'ELO': 31.90
+      },
+      {
+        'name': 'Guanyu Zhou',
+        'image': Image.asset('lib/assets/images/drivers/Guanyu.png'),
+        'isVisible': true,
+        'ELO': 26.50
+      },
+    ];
+  }
+}
