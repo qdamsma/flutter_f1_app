9165b55546adb5b58bdffdf71d3ebc3884ddac4c
Quinten <90333974+qdamsma@users.noreply.github.com>
2024-11-13 14:32:34 +0100

    Kleine aanpassing in kleur

--- lib/pages/teamswapper.dart
+++ lib/pages/teamswapper.dart
@@ -86,7 +86,7 @@ class _TeamswapperState extends State<Teamswapper> {
       appBar: AppBar(
         title: Image.asset('lib/assets/images/f1_logo_app.png', height: 40),
         centerTitle: true,
-        backgroundColor: const Color(0xFFFF1E00),
+        backgroundColor: Color.fromARGB(255, 203, 29, 6),
         elevation: 0.0,
       ),
       body: Column(
@@ -249,7 +249,7 @@ class _TeamswapperState extends State<Teamswapper> {
                         height: 100,
                         width: 100,
                         decoration: BoxDecoration(
-                            color: Colors.redAccent,
+                            color: Color.fromARGB(255, 203, 29, 6),
                             borderRadius: BorderRadius.circular(10)),
                         child: Center(child: driver['image']),
                       ),
